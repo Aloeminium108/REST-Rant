957fead9ce57bb5fab76edc665ae03b462a227df
Alice Parrish <aloeasinthevera@gmail.com>
2023-02-23 16:23:43 -0500

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/bruna-branco-0wn-DdavPa4-unsplash.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/nick-karvounis-jN_M0McVNeI-unsplash.jpg'
+}]
