eb311c94ead6db196f36057153c65790120c89c8
Alice Parrish <aloeasinthevera@gmail.com>
2023-03-06 14:48:42 -0500

    Added helper method to show when/where places were established

---
+++ seeders/seed-places.js
@@ -0,0 +1,25 @@
+const db = require('../models')
+
+db.Place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/bruna-branco-0wn-DdavPa4-unsplash.jpg',
+    founded: 1989
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/nick-karvounis-jN_M0McVNeI-unsplash.jpg',
+    founded: 2020
+}])
+.then(() => {
+    console.log('Success!')
+    process.exit()
+})
+.catch(err => {
+    console.log('Failure!', err)
+    process.exit()
+})
